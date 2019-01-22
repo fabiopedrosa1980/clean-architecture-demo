af7e39f3d4aac9aca9fd69ef4587daac7773c42f
Fabio Pedrosa <fpedrosa@laptec.com.br>
2019-01-22 17:52:42 -0200

    insert getOne and delete

--- tools/check-clean-architecture.sh
+++ tools/check-clean-architecture.sh
@@ -3,10 +3,10 @@
 readonly SRC_PATH="src/main/java/com/example/demo"
 
 readonly DOMAIN_PACKAGE_PATH="${SRC_PATH}/domain"
-readonly USE_CASES_PACKAGE_PATH="${SRC_PATH}/use_cases"
+readonly USE_CASES_PACKAGE_PATH="${SRC_PATH}/usecases"
 
 readonly UNAUTHORIZED_PACKAGES_USAGE_IN_USE_CASES="infrastructure|hibernate"
-readonly UNAUTHORIZED_PACKAGES_USAGE_IN_DOMAIN="${UNAUTHORIZED_PACKAGES_USAGE_IN_USE_CASES}|use_cases|springframework|javax"
+readonly UNAUTHORIZED_PACKAGES_USAGE_IN_DOMAIN="${UNAUTHORIZED_PACKAGES_USAGE_IN_USE_CASES}|usecases|springframework|javax"
 
 readonly UNAUTHORIZED_PACKAGES_USAGE_COUNT_IN_DOMAIN=$(find ${DOMAIN_PACKAGE_PATH} -name "*.java" -exec egrep -w ${UNAUTHORIZED_PACKAGES_USAGE_IN_DOMAIN} {} \; | wc -l)
 readonly UNAUTHORIZED_PACKAGES_USAGE_COUNT_IN_USE_CASES=$(find ${USE_CASES_PACKAGE_PATH} -name "*.java" -exec egrep -w ${UNAUTHORIZED_PACKAGES_USAGE_IN_USE_CASES} {} \; | wc -l)
